a32c2f13e8cf8db085ee232eb6bf4d4251c08654
Raviteja3623 <rgatti1996@gmail.com>
2023-06-12 12:07:23 -0400

    new changes

--- Main.java.java
+++ Main.java.java
@@ -5,6 +5,6 @@ public class Main {
 
         double area = length * width;
 
-        System.out.println("The area of the rectangle is: " + area);
+        System.out.println("The area of the rectangle: " + area);
     }
 }
