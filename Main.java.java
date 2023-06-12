3ddc95a9a38ead5ce1e88c17aec236882d0a9785
Raviteja3623 <rgatti1996@gmail.com>
2023-06-12 12:04:29 -0400

    Main file is committed

---
+++ Main.java.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double length = 5.5;
+        double width = 3.2;
+
+        double area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
